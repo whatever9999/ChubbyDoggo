b4f496febc18fc270830e095e79daac7db9b7904
DominiqueRussell33 <DominiqueRussell33@users.noreply.github.com>
2018-09-20 20:35:11 +0100

    Sorted out collisions and carrot appearance

---
+++ ChubbyDoggo/Assets/Scripts/GameController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public Camera cam;
+    public GameObject carrot;
+    public new Renderer renderer;
+    private float maxWidth;
+
+    // Use this for initialization
+    void Start() {
+        renderer = carrot.GetComponent<Renderer>();
+
+        if (cam == null) {
+            cam = Camera.main;
+        }
+        Vector3 upperCorner = new Vector3(Screen.width, Screen.height, 0.0f);
+        Vector3 targetWidth = cam.ScreenToWorldPoint(upperCorner);
+        float carrotWidth = renderer.bounds.extents.x;
+        maxWidth = targetWidth.x - carrotWidth;
+        StartCoroutine(Spawn());
+    }
+
+    IEnumerator Spawn() {
+        yield return new WaitForSeconds(2.0f);
+        while (true) {
+            Vector3 spawnPosition = new Vector3(Random.Range(-maxWidth, maxWidth), transform.position.y, 0.0f);
+            Quaternion spawnRotation = Quaternion.identity;
+            Instantiate(carrot, spawnPosition, spawnRotation);
+            yield return new WaitForSeconds(Random.Range(1.0f, 2.0f));
+        }
+    }
+}
