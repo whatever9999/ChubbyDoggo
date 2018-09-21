88ef3f16f2ede5eba3e8d31c9a7d5a8367b3cd76
DominiqueRussell33 <DominiqueRussell33@users.noreply.github.com>
2018-09-21 23:10:52 +0100

    Added timer and part of score

--- ChubbyDoggo/Assets/Scripts/GameController.cs
+++ ChubbyDoggo/Assets/Scripts/GameController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
 
@@ -8,6 +9,8 @@ public class GameController : MonoBehaviour {
     public GameObject carrot;
     public new Renderer renderer;
     private float maxWidth;
+    public float timeLeft = 0;
+    public Text timerText;
 
     // Use this for initialization
     void Start() {
@@ -21,15 +24,28 @@ public class GameController : MonoBehaviour {
         float carrotWidth = renderer.bounds.extents.x;
         maxWidth = targetWidth.x - carrotWidth;
         StartCoroutine(Spawn());
+        UpdateText();
+    }
+
+    void FixedUpdate() {
+        timeLeft -= Time.deltaTime;
+        if(timeLeft < 0) {
+            timeLeft = 0;
+        }
+        UpdateText();
     }
 
     IEnumerator Spawn() {
         yield return new WaitForSeconds(2.0f);
-        while (true) {
-            Vector3 spawnPosition = new Vector3(Random.Range(-maxWidth, maxWidth), transform.position.y, 0.0f);
+        while (timeLeft > 0) {
+            Vector3 spawnPosition = new Vector3(Random.Range(-maxWidth + 1, maxWidth - 1), transform.position.y, 0.0f);
             Quaternion spawnRotation = Quaternion.identity;
             Instantiate(carrot, spawnPosition, spawnRotation);
             yield return new WaitForSeconds(Random.Range(1.0f, 2.0f));
         }
+    }
+
+    void UpdateText() {
+        timerText.text = "Time Left:\n" + Mathf.RoundToInt(timeLeft);
     }
 }
