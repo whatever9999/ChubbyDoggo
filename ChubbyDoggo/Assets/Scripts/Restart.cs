c10652e362f3fc7ad1c58b2df6da367e562ad7e6
DominiqueRussell33 <DominiqueRussell33@users.noreply.github.com>
2018-09-23 14:51:57 +0100

    Fixed Restart error

--- ChubbyDoggo/Assets/Scripts/Restart.cs
+++ ChubbyDoggo/Assets/Scripts/Restart.cs
@@ -1,9 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Restart : MonoBehaviour {
     public void RestartGame() {
-        Application.LoadLevel(Application.loadedLevel);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
